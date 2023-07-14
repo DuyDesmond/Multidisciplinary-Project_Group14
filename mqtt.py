defe9abcb388b5b153f2e7384a9d71728273c9f0
Dương Võ Thiên Bảo <135791389+Afihu@users.noreply.github.com>
2023-07-14 12:43:40 +0700

    delete non-use codeline

--- mqtt.py
+++ mqtt.py
@@ -18,7 +18,6 @@ def connected(client):
     client.subscribe("rainsensor")
     client.subscribe("reservoir")
     client.subscribe("tempsensor")
-    client.subscribe("wateramount")
     client.subscribe("schedule")
     print("Server connected ...")
 
@@ -73,8 +72,6 @@ while True:
         time.sleep(1)
         client.publish("on-slash-off", 0)
         time.sleep(1)
-        client.publish("wateramount",0)
-        time.sleep(1)
     # No rain
     else:
         client.publish("tempsensor", temp)
@@ -84,19 +81,5 @@ while True:
         time.sleep(1)
         client.publish("moistsensor", moisture)
         time.sleep(1)
-        
-        if moisture >= 85: 
-            reservoir -= 5
-            client.publish("wateramount",5)           
-        
-        elif moisture <= 60: 
-            reservoir -= 15
-            client.publish("wateramount",15)
-        
-        else: 
-            reservoir -= 10
-            client.publish("wateramount",10)
-        client.publish("reservoir", reservoir)
-    
-    if reservoir <= 0: reservoir = 100
+      
     time.sleep(12)
